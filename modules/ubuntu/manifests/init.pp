c883f4e5e2a0795f4c6d3e95dc89ecbcd5dd5af9
Carl Jackson <carl@avtok.com>
2013-07-20 00:26:12 -0700

    Basic ubuntu module
    I haven't really thought through where things should go yet

---
+++ modules/ubuntu/manifests/init.pp
@@ -0,0 +1,25 @@
+class ubuntu {
+  case $lsbdistcodename {
+    'precise' : {
+      include ubuntu::precise
+    }
+
+    default : {
+      fail("Unsupported release :(")
+    }
+  }
+
+  package {[
+    'build-essential', 'bwm-ng', 'curl', 'dstat', 'gnupg', 'htop', 'iftop',
+    'iotop', 'mtr-tiny', 'netcat-openbsd', 'pv', 'ruby1.9.1', 'rubygems',
+    'screen', 'tcpdump', 'tmux', 'traceroute', 'vim'
+  ]:
+    ensure => installed,
+    require => Exec['apt-get update']
+  }
+
+  exec { 'apt-get update':
+    command => 'apt-get update',
+    refreshonly => true
+  }
+}
