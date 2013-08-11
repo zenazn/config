0f4b1c6cefa8d458dbfea7c3028dc64b14c060b6
Carl Jackson <carl@stripe.com>
2013-08-10 22:51:30 -0700

    Secrets and ssh

---
+++ modules/ssh/manifests/init.pp
@@ -0,0 +1,20 @@
+class ssh {
+  file { '/etc/ssh':
+    ensure => directory;
+  '/etc/ssh/ssh_config':
+    source => 'puppet:///modules/ssh/ssh_config';
+  '/etc/ssh/sshd_config':
+    source => 'puppet:///modules/ssh/sshd_config';
+  '/etc/ssh/ssh_host_rsa_key':
+    content => hiera('/etc/ssh/ssh_host_rsa_key');
+  '/etc/ssh/ssh_host_dsa_key':
+    content => hiera('/etc/ssh/ssh_host_dsa_key');
+  '/etc/ssh/ssh_host_ecdsa_key':
+    content => hiera('/etc/ssh/ssh_host_ecdsa_key');
+  }
+
+  package { 'ssh':
+    ensure => installed,
+    require => Exec['apt-get update']
+  }
+}
