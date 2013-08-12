5577ed2040c1e841361c385bf2ddbcf545ada28d
Carl Jackson <carl@stripe.com>
2013-08-11 17:19:25 -0700

    Nginx module

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,4 +1,5 @@
-class nginx {
+class nginx($sites=[]) {
+
   package { ["nginx", "passenger-common1.9.1"]:
     ensure => installed;
   "passenger-common1.8":
@@ -6,5 +7,18 @@ class nginx {
   }
   file { "/etc/nginx/":
     ensure => directory;
-  "/etc/nginx/k
+  "/etc/nginx/conf.d":
+    ensure => directory;
+  "/etc/nginx/nginx.conf":
+    ensure => present,
+    source => "puppet:///modules/nginx/nginx.conf";
+
+  "/etc/nginx/sites-available":
+    ensure => directory;
+  "/etc/nginx/sites-enabled":
+    ensure => directory;
+  }
+
+  nginx::site { $sites:
+  }
 }
---
+++ modules/nginx/manifests/site.pp
@@ -0,0 +1,19 @@
+define nginx::site($enabled=true) {
+  include nginx
+
+  file { "/etc/nginx/sites-available/$name.conf":
+    ensure => present,
+    source => "puppet:///modules/nginx/sites/$name.conf"
+  }
+
+  if $enabled {
+    file { "/etc/nginx/sites-enabled/$name.conf":
+      ensure => link,
+      target => "/etc/nginx/sites-available/$name.conf"
+    }
+  } else {
+    file { "/etc/nginx/sites-enabled/$name.conf":
+      ensure => absent
+    }
+  }
+}
