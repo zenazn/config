5e64c3d1d072295c612cc7dbd7d3d8f79264b314
Carl Jackson <carl@stripe.com>
2013-08-11 17:40:42 -0700

    Remove default nginx sites, refactor a bit

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -5,18 +5,17 @@ class nginx($sites=[]) {
   "passenger-common1.8":
     ensure => absent
   }
-  file { "/etc/nginx/":
-    ensure => directory;
-  "/etc/nginx/conf.d":
+  file { ["/etc/nginx/", "/etc/nginx/conf.d"]:
     ensure => directory;
   "/etc/nginx/nginx.conf":
     ensure => present,
     source => "puppet:///modules/nginx/nginx.conf";
 
-  "/etc/nginx/sites-available":
+  ["/etc/nginx/sites-available", "/etc/nginx/sites-enabled"]:
     ensure => directory;
-  "/etc/nginx/sites-enabled":
-    ensure => directory;
+
+  ["/etc/nginx/sites-available/default", "/etc/nginx/sites-enabled/default"]:
+    ensure => absent;
   }
 
   nginx::site { $sites:
