86ffd79b82da5886bcfe7b8eef52fa495c7b6dc1
Carl Jackson <carl@stripe.com>
2013-08-18 23:04:03 -0700

    Dovecot

--- modules/mail/manifests/init.pp
+++ modules/mail/manifests/init.pp
@@ -8,6 +8,8 @@ class mail($destinations=[]) {
   file { '/etc/postfix/main.cf':
     content => template('mail/main.cf.erb');
   '/etc/postfix/master.cf':
-    source => 'puppet:///modules/mail/master.cf'
+    source => 'puppet:///modules/mail/master.cf';
+  '/etc/dovecot/conf.d/01-mail-stack-delivery.conf':
+    content => template('mail/01-mail-stack-delivery.conf.erb')
   }
 }
