c81d3f12199ec65fbae7c1031a2f710fb522822d
Carl Jackson <carl@stripe.com>
2013-08-18 22:31:35 -0700

    Grrr puppet

--- modules/mail/manifests/virtual.pp
+++ modules/mail/manifests/virtual.pp
@@ -1,6 +1,6 @@
 class mail::virtual($addresses={}) {
   file { '/etc/postfix/virtual':
-    contents => join(join_keys_to_values($addresses, ' '), "\n")
+    content => join(join_keys_to_values($addresses, ' '), "\n")
   }
   exec { 'postmap /etc/postfix/virtual':
     subscribe => File['/etc/postfix/virtual'],
