fb5ac9e65c743c8718332dab7b15dc90813c5125
Carl Jackson <carl@avtok.com>
2013-09-08 01:39:31 -0700

    Backup MX

--- modules/mail/manifests/relay.pp
+++ modules/mail/manifests/relay.pp
@@ -1,3 +1,11 @@
 class mail::relay {
+  include mail::common
+  include mail::virtual
 
+  package { ['postfix', 'postgrey']:
+  }
+
+  file { '/etc/postfix/main.cf':
+    content => template('mail/backup-main.cf.erb')
+  }
 }
