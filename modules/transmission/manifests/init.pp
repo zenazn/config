1c624f7b56f7716fab3c867a2863a609fd3f0026
Carl Jackson <carl@avtok.com>
2013-09-09 01:02:38 -0700

    Add /var/downloads

--- modules/transmission/manifests/init.pp
+++ modules/transmission/manifests/init.pp
@@ -2,6 +2,11 @@ class transmission {
   package { "transmission-daemon":
   }
   file { "/etc/transmission-daemon/settings.json":
-    source => "puppet:///modules/transmission/settings.json"
+    source => "puppet:///modules/transmission/settings.json";
+  "/var/downloads":
+    ensure => directory,
+    owner => 'debian-transmission',
+    group => 'debian-transmission',
+    mode => '0644'
   }
 }
